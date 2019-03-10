cfb089d9a5b8a06e0d8e5133ff34edae6a30c69a
An idiot <ur@mom.com>
2019-03-10 15:56:34 -0700

    TeaParty

--- src/TeaParty.java
+++ src/TeaParty.java
@@ -1,9 +1,23 @@
 
 public class TeaParty {
 
-	public static void main(String[] args) 
 	
-
+	public String welcome(String name, boolean isWoman, boolean isKnighted) {
+if(isWoman==true) {
+	if(isKnighted==true) {
+	return "Hello Lady "+name;
+	}else {
+	return "Hello Ms. "+name;
 	}
+}
+else {
+	if(isKnighted==true) {
+		return "Hello Sir " +name;
+	}else {
+		return "Hello Mr. " +name;
+	}
+}
+    }
+
 
 }
